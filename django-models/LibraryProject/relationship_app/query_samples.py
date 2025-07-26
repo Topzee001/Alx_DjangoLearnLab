7fcce8b6141de24f972eea50bfe2edc3939e1bc0
Ibrahim <ibrahimsakariyah@gmail.com>
2025-07-26 22:05:06 +0100

    task 1

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,6 +9,6 @@ books = Book.objects.filter(author=author)
 library_name = "library_name"
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-# Retrieve the librarian for a library
 
+# Retrieve the librarian for a library
 librarian = Librarian.objects.get(library=library)
