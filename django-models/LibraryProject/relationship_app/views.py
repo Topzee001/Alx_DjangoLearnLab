d4992b761b4cb102420e1da8832d1b13c200761c
Ibrahim <ibrahimsakariyah@gmail.com>
2025-07-27 03:58:47 +0100

    task 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,11 +9,11 @@ from .models import Library
 # fxn based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # class based view
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
