338bf7c9d643238cc95d4188618f8278d5aa70a7
Ibrahim <ibrahimsakariyah@gmail.com>
2025-08-01 22:48:03 +0100

    w11 task1 step3: enfoece permission in views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 # Create your views here.
+# any group with this permission can view all books
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/list.html', {'books': books})
+
+# any user with permission can create book
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    # # logic to add book
+    # if request.method == 'POST':
+    #     title = request.POST['title']
+    #     author = request.POST['author']
+    #     publication_year = request.POST['publication_year']
+    #     book = Book(title=title, author=author, publication_year=publication_year)
+    #     book.save()
+    #     return redirect('book_list')
+    return render(request, 'books/add.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = Book.objects.get(pk=pk)
+   # # logic to edit book
+    # if request.method == 'POST':
+    #     book.title = request.POST['title']
+    #     book.author = request.POST['author']
+    #     book.publication_year = request.POST['publication_year']
+    #     book.save()
+    #     return redirect('book_list')
+    return render(request, 'books/edit.html', {'books/edit.html': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    # # logic to delete book
+    # book = Book.objects.get(pk=pk)
+    # book.delete()
+    return redirect('book_list')
+
+
+
+
