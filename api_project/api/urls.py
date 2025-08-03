f5198c1082f72b40b9b6e68e89733e843234e64f
Ibrahim <ibrahimsakariyah@gmail.com>
2025-08-03 20:48:39 +0100

    task 2: implementing crud with viewsets and routers in DRF

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
+from rest_framework import routers
+from .views import BookViewSet
 
 # Create your views here.
+router = routers.DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('books/', BookList.as_view(), name='book-list'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 
 # Create your views here.
@@ -9,4 +9,7 @@ from rest_framework import generics
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
