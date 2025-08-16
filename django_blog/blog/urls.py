bc5706f01649556114dd3f8eaf55bd09f9af3649
Ibrahim <ibrahimsakariyah@gmail.com>
2025-08-16 15:12:39 +0100

    blog user auth system

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,7 +5,7 @@ from .views import signup_view, profile_view, profile_update_view
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'), 
-    path('signup/', signup_view, name='signup'),
+    path('register/', signup_view, name='signup'),
     # path('profile/update/', ProfileUpdateView.as_view(), name='profile-update'),
     path('profile-update/', profile_update_view, name='profile-updates'),
     path('profile/', profile_view, name='profile'),
