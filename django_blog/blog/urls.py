4f270aa0a4e58a2f482163bb4493b2146a53aac3
Ibrahim <ibrahimsakariyah@gmail.com>
2025-08-17 13:23:05 +0100

    added forms and routes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('post/', BlogPostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', BlogPostDetailView.as_view(), name='post-detail'),
     path('post/new/', BlogPostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', BlogPostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', BlogPostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', BlogPostDeleteView.as_view(), name='post-delete'),
 ]
