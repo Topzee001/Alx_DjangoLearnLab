72ccb2c02a8ccf1d946618733c8dc67d9d08809f
Ibrahim <ibrahimsakariyah@gmail.com>
2025-08-24 22:19:21 +0100

    w15 task 0, project setup and auth

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -11,7 +11,8 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
+    # password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
