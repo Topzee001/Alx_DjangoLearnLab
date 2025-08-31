991e1095c5bf0fb520bb39e3404fafd4311e57a0
Ibrahim <ibrahimsakariyah@gmail.com>
2025-08-31 07:50:07 +0100

    addded requirement.txt

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -27,9 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kwo!2byh)=7jz_!ixrp_axo-vrh1qzj00ze)4rm_y_kqr)uwxs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    "http://127.0.0.1:8000/"
+]
 
 
 # Application definition
@@ -139,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
